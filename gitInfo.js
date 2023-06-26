a7fe9f49bf0c41a2dc2c85dbe108144bc7cc80cd
Izzyosman <137222951+Izzyosman@users.noreply.github.com>
2023-06-25 21:10:26 -0500

    commit Assessment-1

---
+++ gitInfo.js
@@ -0,0 +1,111 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+let gitDefinition = "Git is a platform where multiple users can collaborate with each other."
+
+console.log(gitDefinition) 
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+let gitHubDefinition = "Github is a web based platform that provides hosting for git repositories and allows for developer collaboration."
+
+console.log(gitHubDefinition)
+
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+let gitInitDefinition = "git init is a command used in Git to initialize a new repository. It is used to track changes."
+
+console.log(gitInitDefinition)
+
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+let gitCloneDefinition = "git clone is used on an existing repository and makes a clone or a copy of that repo." 
+
+console.log(gitCloneDefinition) 
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+let gitStatusDefinition = "The git status command displays the state of the working directory and the staging area. It lets you see which changes have been staged and which have not and which files are not being tracked by Git." 
+
+console.log(gitStatusDefinition) 
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+let gitAddDefinition = "The git add command adds a change in the working directory to the staging area. It tells Git that you want to include updates to a particular file in the next commit."
+
+console.log(gitAddDefinition) 
+
+let gitAddCode = "The git add is a command and which adds changes in the working directory to the staging area. With the help of this command and you tell Git that you want to add updates to a certain file in the next commit. But in order to record changes then you need to run git commit too."
+
+console.log(gitAddCode) 
+
+
+
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+let gitCommitDefinition = "Commits can be thought of as snapshots or milestones along the timeline of a Git project. Git commit saves repository changes on local but not remote repository."
+
+console.log(gitCommitDefinition) 
+
+let gitCommitCode = "commands like commit message is a git commit code."
+
+console.log(gitCommitCode)
+
+
+
+
+
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+let gitPushDefinition = "The git push command is used to upload local repository content to a remote repository. Pushing is how you transfer commits from your local repository to a remote repo."
+
+console.log(gitPushDefinition)
+
---
+++ sdlc.js
@@ -0,0 +1,55 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+// Planning 
+// Analysis of Requirements 
+// Design
+// Implementation 
+// Testing and Integration
+// Maintain 
+
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+console.log(planning)
+
+const analysisOfRequirements = "In this step detailed requirements are gathered analyzed and documented to understand what the software system needs to accomplish."
+
+console.log(analysisOfRequirements) 
+
+const design = "Once the requirements are established the system architecture software design and user interface are planned and documented."
+
+console.log(design) 
+
+const implementation = "This phase involves writing code building the software and implementing the designs according to the specifications."
+
+console.log(implementation)
+
+const testingAndIntegration = "The software is thoroughly tested to ensure it meets the requirements and integrates successfully with other components. Testing helps identify and fix any bugs or issues."
+
+console.log(testingAndIntegration)
+
+const maintain = "After deployment ongoing maintenance activities such as bug fixing updates and enhancements are carried out to ensure the softwares functionality and reliability."
+
+console.log(maintain)
+
+
+
